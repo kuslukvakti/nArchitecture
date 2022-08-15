1763508afee129ea92058ab9ada931b416cd62f4
Düha Başdoğan <duha.basdogan@hotmail.com>
2022-08-15 23:07:06 +0300

    20220815

---
+++ src/demoProjects/rentACar/Application/Services/Repositories/IBrandRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Services.Repositories
+{
+    using Core.Persistence.Repositories;
+    using Domain.Entities;
+
+    public interface IBrandRepository : IAsyncRepository<Brand>, IRepository<Brand>
+    {
+    }
+}
---
+++ src/demoProjects/rentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Persistence.Repositories
+{
+    using Application.Services.Repositories;
+    using Core.Persistence.Repositories;
+    using Domain.Entities;
+    using Persistence.Contexts;
+
+    public class BrandRepository : EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        public BrandRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
