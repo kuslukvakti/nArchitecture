8b6b9ccbba498e6e30e91da22b5f093aeb5c9420
Düha Başdoğan <duha.basdogan@hotmail.com>
2022-08-15 22:15:36 +0300

    20220815

---
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities
+{
+    using Core.Persistence.Repositories;
+
+    public class Brand : Entity
+    {
+        public string Name { get; set; }    
+
+        public Brand()
+        {
+        }
+
+        public Brand(int id, string name): this()
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
