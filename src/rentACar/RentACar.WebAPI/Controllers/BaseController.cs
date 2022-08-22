a3924b5b93f22c0ed9fa05c8f7823ffd657e583e
Düha Başdoğan <duha.basdogan@hotmail.com>
2022-08-22 23:10:38 +0300

    20220821

---
+++ src/rentACar/RentACar.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿namespace RentACar.WebAPI.Controllers
+{
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BaseController : ControllerBase
+    {
+        protected IMediator? Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+        private IMediator? _mediator;
+    }
+}
---
+++ src/rentACar/RentACar.WebAPI/Controllers/BrandController.cs
@@ -0,0 +1,20 @@
+﻿namespace RentACar.WebAPI.Controllers
+{
+    using Application.Features.Brands.Commands.CreateBrand;
+    using Application.Features.Brands.Dtos;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateBrandCommand createSomeFeatureEntityCommand)
+        {
+            CreatedBrandDto result = await Mediator.Send(createSomeFeatureEntityCommand);
+            return Created("", result);
+        }
+
+
+    }
+}
