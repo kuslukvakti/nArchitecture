8bf675aeb160b0d0c3ddbae96c4813015aba7cf3
Düha Başdoğan <duha.basdogan@hotmail.com>
2022-08-22 22:51:27 +0300

    20220821

--- src/rentACar/RentACar.Application/Features/Brands/Profiles/MappingProfiles.cs
+++ src/rentACar/RentACar.Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,4 +1,4 @@
-﻿namespace Application.Features.someFeature.Profiles
+﻿namespace RentACar.Application.Features.Brands.Profiles
 {
     using AutoMapper;
     using RentACar.Application.Features.Brands.Commands.CreateBrand;
